ea1b87dacfe2c9022f090d76ddd3f1b3ed9d1a75
Fahad Hasan <hasan.fahad67@outlook.com>
2025-10-28 22:41:32 +0600

    Ui crud operation done on Expence Category

--- ExpenseManagement.Web/Services/ExpenseCategoryApiService.cs
+++ ExpenseManagement.Web/Services/ExpenseCategoryApiService.cs
@@ -1,5 +1,8 @@
 ﻿using System.Net.Http.Json;
+using ExpenseManagement.Shared.Models;
+using ExpenseManagement.Shared.Models.DtoModels;
 using ExpenseManagement.Shared.Models.ViewModel;
+using ExpenseManagement.Web.Pages.ExpenseCategory;
 
 namespace ExpenseManagement.Web.Services
 {
@@ -15,11 +18,30 @@ namespace ExpenseManagement.Web.Services
         public async Task<List<ExpenseCategoriesViewModel>> GetExpenseCategoriesAsync()
         {
             var response = await _httpClient.GetFromJsonAsync<List<ExpenseCategoriesViewModel>>("ExpenseCategories/AllExpenseCategories");
-            return response ?? new();
+            return response ?? [];
         }
         public async Task<ExpenseCategoriesViewModel> GetExpenseCategoryByIdAsync(long expenseCategoryId)
         {
             var response = await _httpClient.GetFromJsonAsync<ExpenseCategoriesViewModel>($"/ExpenseCategories/GetExpenseCategoriesById/{expenseCategoryId}");
+            return response ?? new();
+        }
+
+        public async Task<ResponseModel> CreateExpenseCategoryAsync(ExpenseCategoriesDto expenseCategoriesDto)
+        {
+            var response = await _httpClient.PostAsJsonAsync("ExpenseCategories/AddExpenseCategories", expenseCategoriesDto);
+            return await response.Content.ReadFromJsonAsync<ResponseModel>() ?? new();
+        }
+
+
+        public async Task<ResponseModel> UpdateExpenseCategoryAsync(ExpenseCategoriesDto expenseCategoriesDto)
+        {
+            var response = await _httpClient.PutAsJsonAsync("ExpenseCategories/UpdateExpenseCategories", expenseCategoriesDto);
+            return await response.Content.ReadFromJsonAsync<ResponseModel>() ?? new();
+        }
+
+        public async Task<ResponseModel> DeleteExpenseCategoriesAsync(long expenseCategoryId)
+        {
+            var response = await _httpClient.DeleteFromJsonAsync<ResponseModel>($"ExpenseCategories/DeleteExpenseCategories/{expenseCategoryId}");
             return response ?? new();
         }
     }
