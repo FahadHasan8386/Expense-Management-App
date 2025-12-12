561405d7e6167cc834e5f6374dd68b4c3720dd76
Fahad Hasan <hasan.fahad67@outlook.com>
2025-12-12 23:59:04 +0600

    Query Search

---
+++ ExpenseManagement.Shared/Models/DtoModels/QueryDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Shared.Enums;
+
+namespace ExpenseManagement.Shared.Models.DtoModels
+{
+    public sealed class QueryDto
+    {
+        public EnumQueryTypes QueryTypes { get; set; }
+        public long ExpenseCategoryId { get; set; }
+        public EnumPaymentType PaymentType { get; set; }
+        public decimal FromAmount { get; set; }
+        public decimal ToAmount { get; set; }
+        public DateTime FromDate { get; set; } = DateTime.Now;
+        public DateTime ToDate { get; set; } = DateTime.Now;
+    }
+}
+
