262eb2f9dea9478568f81700317fce9f9b050b45
Fahad Hasan <hasan.fahad67@outlook.com>
2025-12-13 16:33:36 +0600

    Home Page Add Search Query

--- ExpenseManagement.Shared/Enums/EnumQueryTypes.cs
+++ ExpenseManagement.Shared/Enums/EnumQueryTypes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace ExpenseManagement.Shared.Enums
 {
     public enum EnumQueryTypes
     {
+        [Display(Name = "===Select Query Types===")]
+        NONE = 0,
+        [Display(Name = "Deposit")]
         DepositReport = 1,
+        [Display(Name = "Expense")]
         ExpenseReport = 2
     }
 }
--- ExpenseManagement.Shared/Models/DtoModels/QueryDto.cs
+++ ExpenseManagement.Shared/Models/DtoModels/QueryDto.cs
@@ -10,8 +10,8 @@ namespace ExpenseManagement.Shared.Models.DtoModels
     public sealed class QueryDto
     {
         public EnumQueryTypes QueryTypes { get; set; }
-        public long ExpenseCategoryId { get; set; }
         public EnumPaymentType PaymentType { get; set; }
+        public long ExpenseCategoryId { get; set; }
         public decimal FromAmount { get; set; }
         public decimal ToAmount { get; set; }
         public DateTime FromDate { get; set; } = DateTime.Now;
