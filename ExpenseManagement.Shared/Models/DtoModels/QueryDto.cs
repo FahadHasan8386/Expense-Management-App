350ee221da95fb743c4fee4a43307b730ff2adee
Fahad Hasan <hasan.fahad67@outlook.com>
2026-01-12 23:43:08 +0600

    Add Invoice

--- ExpenseManagement.Shared/Models/DtoModels/QueryDto.cs
+++ ExpenseManagement.Shared/Models/DtoModels/QueryDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace ExpenseManagement.Shared.Models.DtoModels
 {
     public sealed class QueryDto
     {
+        [Required]
         public EnumQueryTypes QueryTypes { get; set; }
         public EnumPaymentType PaymentType { get; set; }
         public long ExpenseCategoryId { get; set; }
